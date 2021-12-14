75b01e1bf1cdfaa7897d9e37fbc24b2031a2b46b
jaimecgomezz <jaimecgomezwork@gmail.com>
2021-12-13 19:44:00 -0600

    feat: Improve cli capabilities

--- src/cli.rs
+++ src/cli.rs
@@ -1,17 +1,21 @@
-use std::fs::File;
-use std::io;
-use std::io::BufRead;
-use std::io::BufReader;
-use std::io::Write;
+use crate::definitions::types::{InputFormat, OutputFormat};
+use std::env::current_dir;
+use std::error::Error;
+use std::fs::{File, OpenOptions};
+use std::io::{stdin, stdout, BufRead, BufReader, Write};
 use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "dft", about = "data files transformer cli")]
+#[structopt(name = "dft", about = "data files transformer")]
 pub struct Cli {
-    /// Reverse evaluate
+    /// Reverse evaluate?
     #[structopt(short, long)]
     pub undo: bool,
+
+    /// Overwrite output
+    #[structopt(long)]
+    pub overwrite: bool,
 
     /// Verbose mode (-v, -vv, -vvv, ...)
     #[structopt(short, long, parse(from_occurrences))]
@@ -36,6 +40,14 @@ pub struct Cli {
     /// Instructions file
     #[structopt(long, short = "z", default_value = "instructions.dft")]
     pub instructions: String,
+
+    /// Logger output, defaults to stdout
+    #[structopt(short, long)]
+    pub logger: Option<String>,
+
+    /// Input and output format, overwrites <from> and <to>
+    #[structopt(short = "F", long)]
+    pub format: Option<String>,
 }
 
 impl Cli {
@@ -43,24 +55,120 @@ impl Cli {
         Self::from_args()
     }
 
-    pub fn instructor(&self) -> Option<Box<dyn BufRead>> {
-        match File::open(&self.instructions) {
-            Err(_) => None,
-            Ok(file) => Some(Box::new(BufReader::new(file))),
+    pub fn iformat(&self) -> Result<InputFormat, Box<dyn Error>> {
+        let mut result = String::from("");
+
+        result = match &self.format {
+            Some(format) => format.to_owned(),
+            None => match &self.from {
+                Some(from) => from.to_owned(),
+                None => match &self.input {
+                    Some(path) => match path.file_name() {
+                        Some(filename) => match filename.to_str() {
+                            Some(fstr) => {
+                                let components: Vec<&str> = fstr.split(".").collect();
+
+                                match components.last() {
+                                    Some(extension) => extension.to_string(),
+                                    None => result,
+                                }
+                            }
+                            None => result,
+                        },
+                        None => result,
+                    },
+                    None => result,
+                },
+            },
+        };
+
+        match result.as_str() {
+            "csv" => Ok(InputFormat::CSV),
+            _ => panic!("Invalid input format"),
         }
     }
 
-    pub fn reader(&self) -> Box<dyn BufRead> {
+    pub fn oformat(&self) -> Result<OutputFormat, Box<dyn Error>> {
+        let mut result = String::from("");
+
+        result = match &self.format {
+            Some(format) => format.to_owned(),
+            None => match &self.to {
+                Some(to) => to.to_owned(),
+                None => match &self.output {
+                    Some(path) => match path.file_name() {
+                        Some(filename) => match filename.to_str() {
+                            Some(fstr) => {
+                                let components: Vec<&str> = fstr.split(".").collect();
+
+                                match components.last() {
+                                    Some(extension) => extension.to_string(),
+                                    None => result,
+                                }
+                            }
+                            None => result,
+                        },
+                        None => result,
+                    },
+                    None => result,
+                },
+            },
+        };
+
+        match result.as_str() {
+            "csv" => Ok(OutputFormat::CSV),
+            _ => panic!("Invalid output format"),
+        }
+    }
+
+    pub fn reader(&self) -> Result<Box<dyn BufRead>, Box<dyn Error>> {
         match &self.input {
-            None => Box::new(BufReader::new(io::stdin())),
-            Some(filename) => Box::new(BufReader::new(File::open(filename).unwrap())),
+            None => Ok(Box::new(BufReader::new(stdin()))),
+            Some(filename) => Ok(Box::new(BufReader::new(File::open(filename)?))),
         }
     }
 
-    pub fn writer(&self) -> Box<dyn Write> {
+    pub fn writer(&self) -> Result<Box<dyn Write>, Box<dyn Error>> {
         match &self.output {
-            None => Box::new(io::stdout()),
-            Some(filename) => Box::new(File::create(filename).unwrap()),
+            None => Ok(Box::new(stdout())),
+            Some(filename) => {
+                let mut path = current_dir()?;
+
+                path.push(filename);
+
+                let file = OpenOptions::new()
+                    .read(true)
+                    .write(self.overwrite)
+                    .create(true)
+                    .open(path)?;
+
+                Ok(Box::new(file))
+            }
+        }
+    }
+
+    pub fn instructor(&self) -> Result<Box<dyn BufRead>, Box<dyn Error>> {
+        let file = File::open(&self.instructions)?;
+
+        Ok(Box::new(BufReader::new(file)))
+    }
+
+    pub fn logger(&self) -> Result<Box<dyn Write>, Box<dyn Error>> {
+        match &self.logger {
+            None => Ok(Box::new(stdout())),
+            Some(filename) => {
+                let mut path = current_dir()?;
+
+                path.push(filename);
+
+                let file = OpenOptions::new()
+                    .read(true)
+                    .write(true)
+                    .create(true)
+                    .open(path)?;
+
+                Ok(Box::new(file))
+            }
         }
     }
 }
